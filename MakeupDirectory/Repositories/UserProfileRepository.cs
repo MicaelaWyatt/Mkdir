0f84524e61c1ccf0c2569aefe8545b48b60c4258
Micaela Colpi <micaelacolpi@gmail.com>
2021-07-26 14:02:02 -0500

    all starter files are added, now to test

---
+++ MakeupDirectory/Repositories/UserProfileRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Configuration;
+using MakeupDirectory.Models;
+using MakeupDirectory.Utils;
+
+namespace MakeupDirectory.Repositories
+{
+    public class UserProfileRepository : BaseRepository, IUserProfileRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+        public UserProfile GetByFirebaseUserId(string firebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], Email, FirebaseUserId
+                          FROM Users
+                         WHERE FirebaseUserId = @FirebaseuserId";
+
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", firebaseUserId);
+
+                    UserProfile userProfile = null;
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        userProfile = new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId")
+                        };
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Users (FirebaseUserId, Name, Email)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@FirebaseUserId, @Name, @Email)";
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", userProfile.FirebaseUserId);
+                    DbUtils.AddParameter(cmd, "@Name", userProfile.Name);
+                    DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
+
+                    userProfile.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        /*
+        public UserProfile GetByFirebaseUserId(string firebaseUserId)
+        {
+            return _context.UserProfile
+                       .Include(up => up.UserType) 
+                       .FirstOrDefault(up => up.FirebaseUserId == firebaseUserId);
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            _context.Add(userProfile);
+            _context.SaveChanges();
+        }
+        */
+    }
+}
---
+++ MakeupDirectory/client/src/Components/Header.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { NavLink as RRNavLink } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import { logout } from '../modules/authManager';
+
+export default function Header({ isLoggedIn }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand tag={RRNavLink} to="/">MKDIR</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        { /* When isLoggedIn === true, we will render the Home link */}
+                        {isLoggedIn &&
+                            <>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to=''>Tag Management</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to=''>Posts</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to=''>My Posts</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to=''>Category Management</NavLink>
+                                </NavItem>
+                            </>
+                        }
+                    </Nav>
+                    <Nav navbar>
+                        {isLoggedIn &&
+                            <>
+                                <NavItem>
+                                    <a aria-current="page" className="nav-link"
+                                        style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                                </NavItem>
+                            </>
+                        }
+                        {!isLoggedIn &&
+                            <>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/login">Login</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/register">Register</NavLink>
+                                </NavItem>
+                            </>
+                        }
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
